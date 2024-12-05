1f651af9e600645c32fd8145a32c6454ae77bbf6
feandrade02 <fe.a.andrade@hotmail.com>
2024-12-04 23:17:12 -0300

    :sparkles: feat: att favoritos

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
@@ -10,7 +11,32 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  buttonToggleValue: string = 'inicio';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Recupera o valor salvo no LocalStorage ao carregar o componente
+    const savedValue = localStorage.getItem('buttonToggleValue');
+    if (savedValue) {
+      this.buttonToggleValue = savedValue;
+      if (this.buttonToggleValue === 'inicio') {
+        this.router.navigate(['/']);
+      }
+      else {
+        this.router.navigate([`/${savedValue}`]);
+      }
+    }
+  }
+
   onSelectionChange(value: string): void {
-    console.log('Selecionado:', value);
+    this.buttonToggleValue = value;
+    localStorage.setItem('buttonToggleValue', value); // Salva o valor no LocalStorage
+    if (this.buttonToggleValue === 'inicio') {
+      this.router.navigate(['/']);
+    }
+    else {
+      this.router.navigate([`/${value}`]);
+    }
   }
 }
